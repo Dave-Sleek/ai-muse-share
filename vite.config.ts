87feb63988edd9e7c4cfea1f7cd01ff34561f8c3
Dave Enyi <enyidavid87@gmail.com>
2025-11-01 01:09:03 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-   base: "/your-repo-name/", // 👈 Add this line (MUST match your GitHub repo name)
+   base: "/ai-muse-share/", // 👈 Add this line (MUST match your GitHub repo name)
   server: {
     host: "::",
     port: 8080,
