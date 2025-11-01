b91190a62a9f73b9b52f4ef445d5642ce57fcfe1
Dave Enyi <enyidavid87@gmail.com>
2025-11-01 01:06:01 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+   base: "/your-repo-name/", // 👈 Add this line (MUST match your GitHub repo name)
   server: {
     host: "::",
     port: 8080,
